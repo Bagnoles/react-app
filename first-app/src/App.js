c9e644d466dcd9f6fcc4740ba6525b25d3d422a8
Anna <anna.mironova89@yandex.ru>
2022-02-14 23:17:04 +0300

    home work 2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,16 +1,58 @@
 import './App.css';
-import Message from "./Message";
+import {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h4>App component</h4>
-          <p>First react app</p>
-      </header>
-        <Message message={"Some text....."}/>
-    </div>
-  );
+    const testMessages = [
+        {
+            text: "first",
+            author: "user"
+        },
+        {
+            text: "last",
+            author: "robot"
+        }
+    ];
+
+    const [messageList, setMessageList] = useState(testMessages);
+    const [textMessage, setTextMessage] = useState("");
+
+    const updateTextMessage = (event) => {
+        setTextMessage(event.target.value);
+    }
+
+    const updateMessageList = () => {
+        setMessageList(messageList => [...messageList, {text: textMessage, author: "user"}]);
+    }
+
+    useEffect(() => {
+        if (messageList[messageList.length - 1].author === "user") {
+            setTimeout(() => {
+                setMessageList(messageList => [...messageList, {text: "text", author: "robot"}]);
+            }, 5000);
+        }
+    }, [messageList]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h4>App component</h4>
+                <p>First react app</p>
+                <ul>
+                    {messageList.map((message) => (
+                            <li>
+                                text: {message.text} <br/>
+                                author: {message.author}
+                                <hr/>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <textarea name="message" cols="30" rows="5" value={textMessage} onChange={updateTextMessage}>Введите сообщение...</textarea>
+                <button onClick={updateMessageList}>Отправить сообщение</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
